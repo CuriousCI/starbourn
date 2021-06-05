d536fe5416dad4c1daf08075d8577e3eec3c8ded
TimIlTom <ittomalupu@gmail.com>
2021-06-05 20:04:45 +0200

    Added compressed and resized images

--- src/App.svelte
+++ src/App.svelte
@@ -6,31 +6,31 @@
 			title: 'Our Story',
 			paragraph:
 				'StarBourn Studios Is A Media Company That Offers 360-Degree Media Solutions We Produce Content For TV Shows, Films, Digital And Offer Unique Brand Solutions',
-			image: '1',
+			image: '1-min',
 		},
 		{
 			title: 'Development \n& Production',
 			paragraph:
 				'TV Formats\nFactual/ Non-Scripted\nFeature Films\nShort Films\nDocumentary\nDigital - OTT',
-			image: '2',
+			image: '2-min',
 		},
 		{
 			title: 'Discover Us',
 			paragraph:
 				'Content Creation\nVideo Production\nPost Production\nCommercial\nCorporate\nPrint & Digital',
-			image: '3',
+			image: '3-min',
 		},
 		{
 			title: 'Imagine Beyond',
 			paragraph:
 				'Creative - Ideation & Planning\nContent Production & Creation\nDistribution & Marketing',
-			image: '4',
+			image: '4-min',
 		},
 		{
 			title: 'Branding',
 			paragraph:
 				'We Provide Entertainment Marketing Services To Forward Thinking Brands',
-			image: '5',
+			image: '5-min',
 		},
 	];
 </script>
@@ -38,10 +38,16 @@
 <main>
 	<Header />
 	{#each sections as section}
+		<!-- <div
+			class="section"
+			style="background-image: url(assets/resized-super-compressed/{section.image}.jpg);"
+		> -->
+
 		<div
 			class="section"
-			style="background-image: url(assets/{section.image}.jpg);"
+			style="background-image: url(assets/resized-compressed-image/{section.image}.jpg);"
 		>
+
 			<div>
 				<h1>{section.title}</h1>
 				<p>{section.paragraph}</p>
