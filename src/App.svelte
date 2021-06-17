08efde8d02643e2d0dfba7a30754f2a0da8b0592
Ionut Cicio <ionutcicio.dev@gmail.com>
2021-06-17 21:53:32 +0200

    converted all page components into Sections

--- src/App.svelte
+++ src/App.svelte
@@ -2,13 +2,9 @@
 	import Header from './Header.svelte';
 	import Home from './sections/Home.svelte';
 	import Storylab from './sections/Storylab.svelte';
-	import Story from './sections/Story.svelte';
-	import Development from './sections/Development.svelte';
-	import Discover from './sections/Discover.svelte';
-	import Beyond from './sections/Beyond.svelte';
-	import Branding from './sections/Branding.svelte';
 	import Footer from './Footer.svelte';
 	import Contacts from './sections/Contacts.svelte';
+	import Section from './Section.svelte';
 
 	let scrollY = 0;
 </script>
@@ -32,11 +28,42 @@
 
 	<Home />
 	<Storylab />
-	<Story />
-	<Development />
-	<Discover />
-	<Beyond />
-	<Branding />
+	<Section class="left" title="Our Story" id="story" img="2">
+		StarBourn Studios Is A Media Company That<br />
+		Offers 360-Degree Media Solutions<br />
+		We Produce Content For TV Shows, Films,<br />
+		Digital And Offer Unique Brand Solutions
+	</Section>
+	<Section
+		class="right"
+		title="Development &<br />Production"
+		id="development"
+		img="3"
+	>
+		TV Formats<br />
+		Factual/ Non-Scripted<br />
+		Feature Films<br />
+		Short Films<br />
+		Documentary<br />
+		Digital - OTT
+	</Section>
+	<Section class="left" title="Discover Us" id="discover" img="4">
+		Content Creation<br />
+		Video Production<br />
+		Post Production<br />
+		Commercial<br />
+		Corporate<br />
+		Print & Digital
+	</Section>
+	<Section class="right" title="Imagine Beyond" id="beyond" img="5">
+		Creative - Ideation & Planning<br />
+		Content Production & Creation<br />
+		Distribution & Marketing
+	</Section>
+	<Section class="left" title="Branding" id="branding" img="8">
+		We Provide Entertainment Marketing<br />
+		Services To Forward Thinking Brands
+	</Section>
 	<Contacts />
 	<Footer />
 </main>
---
+++ src/Section.svelte
@@ -0,0 +1,109 @@
+<article
+	class="{$$props.class}-article"
+	id={$$props.id}
+	style="background-image: url('../assets/compressed/{$$props.img}.jpg');"
+>
+	<section class={$$props.class}>
+		<div>
+			<h1>{@html $$props.title}</h1>
+			<p><slot /></p>
+		</div>
+	</section>
+</article>
+
+<style>
+	article {
+		width: 100%;
+		height: 100vh;
+
+		background-position: center;
+		background-size: cover;
+		background-repeat: no-repeat;
+		background-position-y: -20vh;
+
+		display: flex;
+		align-items: flex-end;
+	}
+
+	h1 {
+		font-size: 48px;
+	}
+
+	p {
+		font-size: 16px;
+	}
+
+	section {
+		width: 100%;
+		height: 40vh;
+
+		display: grid;
+		place-items: center;
+
+		background: linear-gradient(
+			0deg,
+			rgba(0, 0, 0, 1) 50%,
+			rgba(0, 0, 0, 0) 100%
+		);
+	}
+
+	@media (min-width: 992px) {
+		article {
+			align-items: center;
+			background-position: bottom;
+		}
+
+		.right-article {
+			justify-content: flex-end;
+		}
+
+		h1 {
+			font-size: 48px;
+		}
+
+		p {
+			font-size: 16px;
+		}
+
+		section {
+			width: 70vw;
+			height: 100%;
+
+			box-sizing: border-box;
+		}
+
+		.left {
+			justify-content: flex-start;
+
+			background: linear-gradient(
+				90deg,
+				rgba(0, 0, 0, 1) 50%,
+				rgba(0, 0, 0, 0) 100%
+			);
+
+			padding-left: 5vw;
+		}
+
+		.right {
+			width: 70vw;
+			height: 100%;
+
+			justify-content: flex-end;
+
+			background: linear-gradient(
+				270deg,
+				rgba(0, 0, 0, 1) 50%,
+				rgba(0, 0, 0, 0) 100%
+			);
+
+			box-sizing: border-box;
+			padding-right: 5vw;
+		}
+	}
+
+	@media (min-width: 1200px) {
+		h1 {
+			font-size: 100px;
+		}
+	}
+</style>
