2fb5f4569af385fbfb3af403cc462ca0e13a63a8
Ionut Cicio <ionutcicio.dev@gmail.com>
2021-08-28 14:39:15 +0200

    used tailwind inside footer

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,4 +1,4 @@
-<footer>
+<footer class="h-16 flex flex-col justify-center items-center bg-black">
     <p>Copyright @2021 The Story Lab, a dentsu company</p>
     <div>
         <span>Privacy policy</span> |
@@ -9,27 +9,3 @@
         <span>Sitemap</span>
     </div>
 </footer>
-
-<style>
-    footer {
-        height: 50px;
-
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-
-        gap: 5px;
-
-        background-color: #222222;
-    }
-
-    p {
-        font-size: 15px;
-    }
-
-    div,
-    span {
-        font-size: 10px;
-    }
-</style>
