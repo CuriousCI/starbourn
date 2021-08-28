0437506b14a17494c0bfd9fdc713ab8630d1fed0
Ionut Cicio <ionutcicio.dev@gmail.com>
2021-08-28 17:18:11 +0200

    updated hom with tailwind

--- src/sections/Home.svelte
+++ src/sections/Home.svelte
@@ -1,12 +1,11 @@
-<!-- <div id={$$props.id} class="window"> -->
 <div id={$$props.id} class="window grid grid-rows-3 items-center">
-    <h1 class="row-span-2">
+    <h1 class="row-span-2 text-3xl md:self-center md:text-6xl xl:text-7xl">
         We bring ideas to life!<br />
         Stories to entertain the<br />
         audience around the world
     </h1>
-    <p class="row-span-1 self-start">
-        "Storytelling is the most powerful way to put<br />
+    <p class="row-span-1 text-base self-start md:text-2xl">
+        "Storytelling is the most powerful way to put<br class="md:hidden" />
         ideas into the world today." --Robert McKee
     </p>
 </div>
@@ -14,40 +13,5 @@
 <style>
     .window {
         background-image: url('../assets/compressed/1.jpg');
-
-        /* display: grid;
-        grid-template-rows: 8.5fr 1fr;
-        place-items: center; */
-    }
-
-    h1 {
-        font-size: 27px;
-    }
-
-    p {
-        font-size: 15px;
-    }
-
-    @media (min-width: 992px) {
-        h1 {
-            justify-self: flex-start;
-
-            font-size: 70px;
-            margin-left: 5vw;
-        }
-
-        p {
-            font-size: 26px;
-        }
-
-        p br {
-            display: none;
-        }
-    }
-
-    @media (min-width: 1200px) {
-        h1 {
-            font-size: 80px;
-        }
     }
 </style>
